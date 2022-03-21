121f962f2d7f402ba205acb7366540165b0058a5
ericgros <ericg@LAPTOP-MI0RV32S>
2022-03-21 17:44:17 +0100

    Puse los metodos

---
+++ Practica10_Eric/src/practica10_eric/Practica10_Eric.java
@@ -0,0 +1,20 @@
+package practica10_eric;
+
+public class Practica10_Eric {
+
+    public static void main(String[] args) {
+     
+        String M = "MARAVILLOSO";
+        
+        System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
+ 
+        
+        practica10_eric.Metodes.ParteSuperior();
+        practica10_eric.Metodes.ParteInferior();
+      
+        
+    }    
+        
+}
+    
+
