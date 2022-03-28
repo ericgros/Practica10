6e9321393ad3ce3c05aaf088941183ab0f64e29d
ericg <ericg@LAPTOP-MI0RV32S>
2022-03-28 17:32:26 +0200

    puse otro sout

--- Practica10_Eric/src/practica10_eric/Practica10_Eric.java
+++ Practica10_Eric/src/practica10_eric/Practica10_Eric.java
@@ -7,11 +7,11 @@ public class Practica10_Eric {
         String M = "MARAVILLOSO";
         
         System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
- 
         
         practica10_eric.Metodes.ParteSuperior();
         practica10_eric.Metodes.ParteInferior();
       
+        System.out.println("6. Comproba aquest petit canvi");
         
     }    
         
